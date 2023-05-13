97106611d280a00643f161e295c637d9d0564505
Bhavana Raja <127816986+bhavanar02@users.noreply.github.com>
2023-05-12 19:01:51 -0700

    Added Valid Parenthese

---
+++ ValidParenArrayPy.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(s):
+        pair = dict(('()', '[]', '{}'))
+        st = []
+        for x in s:
+            if x in '([{':
+                st.append(x)
+            elif len(st) == 0 or x != pair[st.pop()]:
+                return False
+        return len(st) == 0
+    
+    print(isValid("()"))
